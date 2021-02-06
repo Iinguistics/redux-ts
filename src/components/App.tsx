8aa1e655adde0701754b8cb3b5fc726849faa308
Linguistics <thylinguistics@gmail.com>
2021-02-05 22:41:21 -0800

    ...

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,11 @@
 import RepoList from './RepoList';
-
+import '../styles/App.css';
 
 const App: React.FC = ()=>{
   
 
-
-
     return(
-        <div>
+        <div className="container">
           <h1>Search for a package</h1>
           <RepoList />
         </div>
